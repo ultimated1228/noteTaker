b61eb7862624a525dcc5fcd6e3b11fbb01c432f9
ultimated1228 <stevenlucasmeyer@gmail.com>
2023-09-23 17:52:02 -0600

    adding the delete button functionality to the api route.  Still need to develop Readme and push to heroku to host

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -29,4 +29,31 @@ router.post('/notes', (req, res) => {
     });
 });
 
+// ADDITIONAL CHALLENGE DELETE A NOTE BY ID
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        // Find the index of the note with the specified ID
+        const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+        if (noteIndex !== -1) {
+            // Remove the note from the array
+            notes.splice(noteIndex, 1);
+
+            // Put the new notes array back 
+            fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json({ message: 'Note deleted' });
+            });
+        } else {
+            res.status(404).json({ message: 'Note with this ID not found' });
+        }
+    });
+});
+
 module.exports = router;
